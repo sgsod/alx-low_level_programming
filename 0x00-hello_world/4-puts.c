56e84a09a4eb904b0fcfbd8a5c0b495525155cff
sgsod <gamanorman3@gmail.com>
2023-12-15 01:50:12 +0200

    add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - print string with puts()
+ * Return: always 0
+ */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return (0);
+}
