d884f703f51ac525ff47928a94e523db1357b3ca
sgsod <gamanorman3@gmail.com>
2023-12-15 05:40:52 +0200

    101-quote.c Hot-fix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * write - send message to stdout
+ * @fdesc: file description
+ * @buf: string array
+ * @nbyte: bytes used
+ * 
+ * Return: always 1
+ */
+int write(int fdesc, const char *buf, unsigned int nbyte);
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+}
