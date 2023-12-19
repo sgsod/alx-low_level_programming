bda0bdb81079156c38793e3821890983322be058
sgsod <gamanorman3@gmail.com>
2023-12-19 04:47:15 +0200

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * Description: print possible combinations of two digits
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+	short a, b;
+
+	a = b = '0';
+	while (a <= '8')
+	{
+		while (b <= '9')
+		{
+			if (b > a && b != a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a < '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar(10);
+
+	return (0);
+}
+
+
+		putchar(alph);
+		if (alph < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		alph++;
+	}
+	putchar(10);
+
+	return (0);
+}
+
