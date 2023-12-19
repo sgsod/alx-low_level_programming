d16d6d3f04120d91f495cdf5f6c5d7a9a2d2c764
sgsod <gamanorman3@gmail.com>
2023-12-19 02:59:18 +0200

    add 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * Description: print alphabet with putchar
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+	char alph;
+
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		if (alph != 'q' && 'e')
+		{
+		putchar(alph);
+		alph++;
+		}
+	}
+	putchar(10);
+
+	return (0);
+}
+
