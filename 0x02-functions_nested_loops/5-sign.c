f5cca003b7ddae1518ad479c6ff41394cb2d2be5
sgsod <gamanorman3@gmail.com>
2024-01-03 12:48:35 +0200

    new function: print_sign()

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_sign - print sign of number
+  * @n: number to be read
+  *
+  * Return: 1 if n is positive, 0 if zero, -1 if negative
+  */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
