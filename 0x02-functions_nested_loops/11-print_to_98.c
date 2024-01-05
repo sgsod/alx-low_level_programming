411391b3b03cab9d5d4d22b8e8f0fab5570f4661
sgsod <gamanorman3@gmail.com>
2024-01-06 01:59:23 +0200

    Debugging Function: print_to_98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * print_to_98 - prints all natural numbers from n to 98
+  * @n: number to begin from
+  */
+
+void print_to_98(int n)
+{
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	printf("%d\n", n);
+}
