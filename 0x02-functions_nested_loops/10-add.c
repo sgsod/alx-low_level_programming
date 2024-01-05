75a2900d1aa932a46acdc6c7729a15830d764f3c
sgsod <gamanorman3@gmail.com>
2024-01-05 13:29:56 +0200

    New Function: add()

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/**
+  * add - add 2 integers and return the sum
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: sum
+  */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
