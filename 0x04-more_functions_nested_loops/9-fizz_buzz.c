335b3a6ae2e5aa27a88820905a63f73079675de8
sgsod <gamanorman3@gmail.com>
2024-01-07 18:08:10 +0200

    Debugging progam#6: 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,7 +19,7 @@ int main(void)
 		else if (n % 5 == 0 && n % 3 != 0)
 			printf("Buzz ");
 
-		else if (n % 5 == 0 && (n % 3 == 0 && n == 0))
+		else if (n % 5 == 0 && n % 3 == 0)
 			printf("FizzBuzz ");
 
 		else
