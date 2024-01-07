1d05022d335a55a407f348e2b1cb0692d3212672
sgsod <gamanorman3@gmail.com>
2024-01-07 19:29:05 +0200

    New Function: print_triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * print_triangle - print a triangle using '#'
+  * @size: size of triangle
+  */
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size > 0)
+		for (a = 0; a > size; a++)
+		{
+			for (b = 1; b < size - a; b++)
+			{
+				_putchar(' ');
+			}
+			for (c = 0; c <= a; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
