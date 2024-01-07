9bff322f52781d6609afc8634c6c1cac85b5c7f4
sgsod <gamanorman3@gmail.com>
2024-01-07 17:12:16 +0200

    New Function: print_square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  * print_square - print square with '#' character
+  * @size: size of square
+  */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
