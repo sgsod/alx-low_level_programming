292350dc7b4a83318ace1d0af15c091cae6a90f6
sgsod <gamanorman3@gmail.com>
2024-01-07 19:35:31 +0200

    Debugging Function: print_triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,7 +11,8 @@ void print_triangle(int size)
 	if (size > 0)
 		for (a = 0; a > size; a++)
 		{
-			for (b = 1; b < size - a; b++)
+			c = size - a
+			for (b = 1; b < c; b++)
 			{
 				_putchar(' ');
 			}
