72ef3f754acc17760f80574c3a895b1466b1efc1
sgsod <gamanorman3@gmail.com>
2024-01-07 16:33:05 +0200

    New Function: print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * print_diagonal - print diagonal line
+  * @n: amount of '\'
+  */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
