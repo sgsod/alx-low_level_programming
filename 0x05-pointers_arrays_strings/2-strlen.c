6253dcb7ad9a114459d4c80cb240e46e3e5b49b1
sgsod <gamanorman3@gmail.com>
2024-01-10 17:05:08 +0200

    New Function: _strlen()

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+  * _strlen - calculate length of string pointed to
+  * @s: address of string
+  *
+  * Return: length of string
+  */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; *(s + a) != \0; a++)
+	{
+	}
+	return (a);
+}
