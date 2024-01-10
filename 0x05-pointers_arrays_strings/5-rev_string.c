b0fbb72e1a60d3e092be685a63b92c5a000958d4
sgsod <gamanorman3@gmail.com>
2024-01-11 00:48:04 +0200

    debugging Function #6: rev_string()

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,20 +6,19 @@
 
 void rev_string(char *s)
 {
-	int length = sizeof(s);
-	char str[length];
+	char *str
 	int b;
 	int c;
 
+	str = s;
 	for (b = 0; *(s + b) != '\0'; b++)
 	{
-		*(str + b) = *(s + b);
 	}
 	b--;
 	c = 0;
 	while (b >= 0)
 	{
-		*(s + c) = *(str + b);
+		(s + c) = (str + b);
 		c++;
 		b--;
 	}
