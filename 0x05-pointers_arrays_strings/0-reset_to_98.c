4beb098038d2b5449d7680c7555bdea66af2e133
sgsod <gamanorman3@gmail.com>
2024-01-10 15:23:19 +0200

    New Function: reset_to_98()

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+  * reset_to_98 - takes pointer to an int and
+  * change value of int it points at to 98.
+  * @n: address pointed to
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,2 @@
+/* header file 0x05 Pointers, Arrays and Strings project */
+void reset_to_98(int *n);
