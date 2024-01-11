e9371f369bbdc068a89419f2ad0d82820e3f5a8b
sgsod <gamanorman3@gmail.com>
2024-01-11 10:13:26 +0200

    Debugging #1 Function: print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,12 @@ void print_array(int *a, int n)
 {
 	int b;
 
-	for (b = 0; b < (n - 1); b++)
+	if (n > 0)
 	{
-		printf("%d, ", *(a + b));
+		for (b = 0; b < (n - 1); b++)
+		{
+			printf("%d, ", *(a + b));
+		}
 	}
 	printf("%d\n", *(a + b));
 }
