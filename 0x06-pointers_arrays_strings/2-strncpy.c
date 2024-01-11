95a57e0b9a6f9b62bfe16c69886d30db7ddccc94
sgsod <gamanorman3@gmail.com>
2024-01-11 13:48:58 +0200

    New Function: _strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+/**
+  * _strncpy - copy string
+  * @dest: string to be pasted to
+  * @src: source string
+  * @n: number of characters to be copied from source
+  *
+  * Return: pointer to dest
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int b;
+
+	for (b = 0; b < n && src[b] != '\0'; b++)
+	{
+		dest[b] = src[b];
+	}
+	dest[b] = '\0';
+
+	return (dest);
+}
