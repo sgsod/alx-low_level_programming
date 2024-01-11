b0853f0893d2ceeac1a84f21f762b9473318daaf
sgsod <gamanorman3@gmail.com>
2024-01-11 13:57:14 +0200

    Debugging #0 Function: _strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int b;
 
 	for (b = 0; b < n && src[b] != '\0'; b++)
-	{
 		dest[b] = src[b];
-	}
-	dest[b] = '\0';
+
+	for (; b < n; b++)
+		dest[b] = '\0';
 
 	return (dest);
 }
