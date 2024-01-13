61b44ee1315ab34bd38d8cbb826bf6969ddf9e51
sgsod <gamanorman3@gmail.com>
2024-01-14 01:44:49 +0200

    Debugging: 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,18 @@
 /**
   * _strcmp - compare 2 strings
   * @s1: first string
-  * @s2: secon string
+  * @s2: second string
   *
-  * Return: return -1 if s1 < s2, 0 if s1 == s2 and 1 if s1 > s2
+  * Return: return - if s1 < s2, 0 if s1 == s2 and + if s1 > s2
   */
 
 int _strcmp(char *s1, char *s2)
 {
-	int a, b;
+	int b;
 
-	for (b = 0; s1[b] != '\0'; b++)
-		;
-	for (a = 0; s2[a] != '\0'; a++)
-		;
+	for (b = 0; s1[b] == s2[b]; b++)
+	{
+	}
 
-	return (b - a);
+	return (*s1[b] - *s2[b]);
 }
