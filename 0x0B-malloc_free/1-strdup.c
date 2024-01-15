0e5ed89f720a481ca709f8b585c3ad459f6f1644
sgsod <gamanorman3@gmail.com>
2024-01-15 22:18:19 +0200

    New Function: _strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+/**
+  * _strdup - creates copy of string
+  * @str: original string
+  *
+  * Return: NULL if str is NULL or memory is insufficient,
+  * return pointer to duplicate string
+  */
+
+char *_strdup(char *str)
+{
+	int a, b;
+	char *cstr;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (a = 0; str[a] != '\0'; a++) /* get size of str */
+		;
+
+	cstr = malloc(sizeof(*str) * a);
+	if (cstr == NULL)
+		return (NULL);
+	for (b = 0; b < a; b++)
+	{
+		cstr[b] = str[b];
+	}
+	return (cstr);
+}
