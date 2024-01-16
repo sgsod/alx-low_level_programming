7a19c19e9b4edeb557ecf9f0d2b0a77e30b4da42
sgsod <gamanorman3@gmail.com>
2024-01-16 11:14:16 +0200

    New Function: alloc_grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+  * alloc_grid - allocate 2D array of int
+  * @w: width of grid
+  * @h: height of grid
+  *
+  * Return: ponter to grid, NULL if w or h is 0 or negative
+  */
+
+int **alloc_grid(int w, int h)
+{
+	int len = 0, len1, len2;
+	int *arr, **arr_2d;
+
+	if (w <= 0 || h <= 0)
+		return (NULL);
+
+	arr = malloc(sizeof(int) * (w * h));
+	arr_2d = malloc(sizeof(int*) * h);
+
+	for (len1 = 0; len1 < h; len1++)
+	{
+		arr_2d[len1] = arr + len;
+		for (len2 = 0; len2 < w; len2++)
+		{
+			*(arr + len) = 0;
+			len++;
+		}
+	}
+	return (arr_2d);
+}
