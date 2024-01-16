25835d42d0bcb1c06b578fb57488aae9067ee7d2
sgsod <gamanorman3@gmail.com>
2024-01-16 04:20:59 +0200

    Debugging #3: str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,28 +12,28 @@ char *str_concat(char *s1, char *s2)
 	unsigned int a, b, c = 0;
 	char *arr;
 
+/* convert NULL string to empty string */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
+/* get size of arrays combined */
 	for (a = 0; s1[a] != '\0'; a++)
 		c++;
-	for (b = 0; s2[a] != '\0'; b++)
+	for (b = 0; s2[b] != '\0'; b++)
 		c++;
 
-	arr = malloc(sizeof(char) * ++c);
+	arr = malloc(sizeof(char) * (c + 1));
 	if (arr == NULL)
 		return (NULL);
 
 	for (a = 0; s1[a] != '\0'; a++)
 		arr[a] = s1[a];
-
 	for (b = 0; s2[b] != '\0'; b++)
 	{
 		arr[a] = s2[b];
 		a++;
 	}
-
 	return (arr);
 }
