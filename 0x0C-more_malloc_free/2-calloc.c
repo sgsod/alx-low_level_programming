91bac3bdd802e4695f580685406838b70b6a1f01
sgsod <gamanorman3@gmail.com>
2024-01-18 23:47:24 +0200

    2-calloc.c: custome calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+/**
+  * _calloc - allocate memory for an array
+  * Description: allocates memory for an array of nmemb
+  *              elements of size bytes each and returns
+  *              a pointer to the allocated memory.
+  * @nmemb: number of elements
+  * @size: size of elements
+  *
+  * Return: pointer to allocated mem, NULL if malloc fails
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *c;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	
+	c = malloc(nmemb * size);
+	if (c == NULL)
+		return (NULL);
+	*c = 0;
+	return (c);
+}
