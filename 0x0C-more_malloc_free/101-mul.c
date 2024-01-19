613b864fa6bb02748c3e30196132248421f6bb04
sgsod <gamanorman3@gmail.com>
2024-01-19 03:18:22 +0200

    101-mul.c: multiply two positive numbers

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int _isdigit(int argc, char *argv[]);
+/**
+  * main - multiply two numbers
+  * @argc: number of arguments
+  * @argv: arguments
+  *
+  * Return: 0 success, 1 failure
+  */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	_isdigit(argc, argv);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+	printf("%d\n", mul);
+
+	return (0);
+}
+/**
+  * _isdigit - check if input is digit
+  * @argc: number of arguments
+  * @argv: arguments
+  *
+  * Return: 0 success, 98 failure
+  */
+
+int _isdigit(int argc, char *argv[])
+{
+	int index, j;
+	for (index = 1; index < argc; index++)
+	{
+		for (j = 0; argv[index][j] != '\0'; j++)
+		{
+			if (!(argv[index][j] >= '0' && argv[index][j] <= '9'))
+			{
+				printf("Error\n");
+				exit (98);
+			}
+		}
+	}
+
+	return (0);
+}
