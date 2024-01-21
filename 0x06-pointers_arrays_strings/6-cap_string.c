5adc1030ffa8c00eb4a375583d70fcce862f057d
sgsod <gamanorman3@gmail.com>
2024-01-21 22:34:58 +0200

    6-cap_string.c: capitalize all words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+/**
+  * cap_string - convert capitalize words in string
+  * @str: string
+  *
+  * Return: pointer to string
+  */
+
+char *cap_string(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		if (str[n] >= 'a' && str[n] <= 'z')
+		{
+			if (str[n - 1] == '.' || str[n - 1] == ',' || str[n - 1] == ';'
+					|| str[n - 1] == '!' || str[n - 1] == '?' || str[n - 1] == '"'
+					|| str[n - 1] == '(' || str[n - 1] == ')' || str[n - 1] == '{'
+					|| str[n - 1] == '\n' || str[n - 1] == '}' || str[n - 1] == ' '
+					|| str[n - 1] == '\t')
+				str[n] -= 32;
+		}
+	}
+	return (str);
+}
