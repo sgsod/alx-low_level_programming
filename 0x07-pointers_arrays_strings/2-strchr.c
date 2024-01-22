60c375ae9f22db94f2d3a51c3348ee144d8c2417
sgsod <gamanorman3@gmail.com>
2024-01-22 03:44:57 +0200

    2-strchr.c: search character from string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+  * _strchr - find character in string
+  * @s: string
+  * @c: character to search
+  *
+  * Return: NULL if not found, pointer to first occurence of c
+  */
+char *_strchr(char *s, char c)
+{
+	int n;
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		if (s[n] == c)
+			return (&s[n]);
+	}
+	return ((char *) 0);
+}
