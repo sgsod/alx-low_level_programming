61986c378bc799c29c8d5b01bde077a8e5450079
sgsod <gamanorman3@gmail.com>
2024-01-22 03:09:29 +0200

    0-memset.c: Debugging

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,14 @@
 /**
   * _memset - fills specified memory with aconstant byte
   * @s: pointer ro memory to be filled
-  * @b: constant\
+  * @b: constant
   * @n: number of bytes to fill
   *
   * Return: pointer to s
   */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int a;
+	unsigned int a;
 
 	for (a = 0; a < n; a++)
 	{
