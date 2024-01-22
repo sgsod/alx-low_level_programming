c82f19d811ca8d77b056417b52a2e2118ae35187
sgsod <gamanorman3@gmail.com>
2024-01-22 23:44:35 +0200

    New file: dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+  * struct dog - structure of name, age and owner of dog's info
+  * @name: name of dog.
+  * @owner: dog owner.
+  * @age: age of dog.
+  */
+#ifndef DOG_H
+#define DOG_H
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif /* DOG_G */
