bc4ea07750286d2ea963624f8d0f4cc8801a7c36
sgsod <gamanorman3@gmail.com>
2024-01-23 04:28:22 +0200

    Debugging #3: 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,14 +10,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	dog = malloc(sizeof(dog_t) - sizeof(float));
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 	if (name != NULL)
 		dog->name = name;
 	if (owner != NULL)
 		dog->owner = owner;
-	(void) age;
+	dog->age = age;
 	return (dog);
 }
   
