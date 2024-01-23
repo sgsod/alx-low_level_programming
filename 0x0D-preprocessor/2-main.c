5236eab0d9f438632ded1e2cf601475fd5183ce7
sgsod <gamanorman3@gmail.com>
2024-01-23 23:36:31 +0200

    complete project

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - print name of source code file
+  *
+  * Return: always 0 (success)
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
