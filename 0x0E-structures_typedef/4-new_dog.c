84bcff96f6e74c170490b669328fb13f816da15f
sgsod <gamanorman3@gmail.com>
2024-01-23 04:13:02 +0200

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+  * new_dog - store a copy of name and owner
+  * @name: dog's name
+  * @age: age of dog
+  * @owner: owner of dog
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t dog;
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	if (name != NULL)
+		dog.name = name;
+	if (owner != NULL)
+		dog.owner = owner;
+	dog.age = age;
+	return (dog);
+}
+  
