d6bd46f417a3c742d048b13e3702e1c832598afc
sgsod <gamanorman3@gmail.com>
2024-01-24 22:21:25 +0200

    New Function: 1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+/**
+  * array_iterator - execute function on each element of an array
+  * @size: size of array
+  * @action: pointer to function to be executed
+  * @array: pointer to array
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t n;
+
+	for (n = 0; n < size; n++)
+		action(array[n]);
+}
