4676e8e0572b9a66b1dba7360326514add9e3ef3
sgsod <gamanorman3@gmail.com>
2024-01-25 18:33:36 +0200

    New Function: 2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * print_strings - print strings followed by new line
+  * @separator: string separator
+  * @n: number of args
+  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int c;
+	char *str;
+	va_list args;
+
+	if (n == 0)
+		return;
+	va_start(args, n);
+
+	for (c = 0; c < n; c++)
+	{
+		str = va_arg(args, char *);
+
+		if (!str)
+			str = "(nil)";
+
+		printf("%s", str);
+
+		if ((separator && *separator != '\0') && c != (n - 1))
+			printf("%s", separator);
+	}
+	putchar('\n');
+	va_end(args);
+}
