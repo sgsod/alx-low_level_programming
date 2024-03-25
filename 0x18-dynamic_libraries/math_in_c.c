ea49959626c762cdf7d9162e038e023cb3687b15
sgsod <gamanorman3@gmail.com>
2024-03-26 00:36:35 +0200

    #advanced - 100

---
+++ 0x18-dynamic_libraries/math_in_c.c
@@ -0,0 +1,60 @@
+#include "main.h"
+/**
+  * add - add two integers
+  * @a: first integer
+  * @b: second integer
+  *
+  * Return: sum of integers
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference of integers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product of integers
+ */
+int mul(int a, int b)
+{
+	        return (a * b);
+}
+
+/**
+ * div - divide two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: quotient of integers
+ */
+int div(int a, int b)
+{
+	        return (a / b);
+}
+
+/**
+ * mod - module two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder of integers
+ */
+int mod(int a, int b)
+{
+	        return (a % b);
+}
